9f9139fea5439f74abda9d30831e181ecbb70cf0
Ales2003 <ales_2003@mail.ru>
2016-02-29 08:14:48 +0300

    nine commit

--- src/javamassiv/JavaMassiv.java
+++ src/javamassiv/JavaMassiv.java
@@ -14,17 +14,36 @@ public class JavaMassiv {
     /**
      * @param args the command line arguments
      */
-    public void print (){
+    public static void print (){
     
-        System.out.println("***");
-        System.out.println("***");
-        System.out.println("***");
-        System.out.println("***");
+        System.out.println("****************************");
         
     }
+    public static void p(int n){
+    System.out.print(n);
+            }
     
     public static void main(String[] args) {
-        // TODO code application logic here
+        int []a=new int[10];
+        
+        for(int i=0;i<a.length-1;i++){
+                System.out.print(a[i]);
+        }
+        System.out.println();
+        print();
+        
+        for(int i=0;i<a.length-1;i++){
+                a[i]=i;
+                JavaMassiv.p(a[i]);
+        
+        }
+        System.out.println();
+        print();
+        
+        
+
+
+// TODO code application logic here
     }
     
 }
